fb301da8ab9525da644ee6e8606efa99bc6537c0
viral4854 <contact.vdedaniya@gmail.com>
2024-11-03 01:11:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+import qr from "qr-image"
+import fs from "fs"
+
+
+inquirer
+  .prompt([
+    {
+        message: "Type your URL here !",
+        name: "URL",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
